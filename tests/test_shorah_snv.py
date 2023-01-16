b205fdca4dee4def7fecfe648984f1c6fc5a9246
Benjamin Langer <blanger@student.ethz.ch>
2023-01-17 00:52:13 +0100

    fix(shorah_snv): Test spec corrected - test expected to fail now

--- tests/test_shorah_snv.py
+++ tests/test_shorah_snv.py
@@ -6,7 +6,7 @@ from shorah.shorah_snv import _compare_ref_to_read, SNP_id, SNV
     ("AACTTA", "AGA--A", {
         SNP_id(pos=2, var='G'): SNV(chrom='HXB2', haplotype_id='some-id', pos=2, ref='A', var='G', freq=1, support=0.1),
         SNP_id(pos=3, var='A'): SNV(chrom='HXB2', haplotype_id='some-id', pos=3, ref='C', var='A', freq=1, support=0.1),
-        SNP_id(pos=4, var='--'): SNV(chrom='HXB2', haplotype_id='some-id', pos=3, ref='CTT', var='C', freq=1, support=0.1)
+        SNP_id(pos=4, var='--'): SNV(chrom='HXB2', haplotype_id='some-id', pos=3, ref='CTT', var='A', freq=1, support=0.1)
     }),
     ("AA--G", "A---T", {
         SNP_id(pos=2, var='---'): SNV(chrom='HXB2', haplotype_id='some-id', pos=1, ref='AA--', var='A', freq=1, support=0.1),
