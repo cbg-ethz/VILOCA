0020fa262a5cbbee7045f2abefc3948c85d1b46d
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-05-02 15:40:34 +0200

    Fix b2w (#35)
    * return to before, make ref2binary okay for Ns
    
    * remove duplicate function update_tiling()
    
    * add new variable original_minimum_overlap for exteneded_window_mode since last_aligned_pos is in orignal reference genome space

--- viloca/b2w.py
+++ viloca/b2w.py
@@ -157,6 +157,7 @@ def _run_one_window(samfile, window_start, reference_name, window_length,control
     # TODO: window_length
     original_window_length = window_length
     window_length = control_window_length
+    original_minimum_overlap = minimum_overlap
     if extended_window_mode:
         # this is now done intilaly for all windows
         #for pos, val in max_ins_at_pos.items():
@@ -208,7 +209,8 @@ def _run_one_window(samfile, window_start, reference_name, window_length,control
 
 
         if (first_aligned_pos + minimum_overlap < window_start + 1 + window_length
-                and last_aligned_pos >= window_start + minimum_overlap - 2 # TODO justify 2
+                and last_aligned_pos >= window_start + original_minimum_overlap - 2 # TODO justify 2
+                #last_aligned_pos: is in the orginal reference genome space (not in the extended_window_mode-space)
                 and len(full_read) >= minimum_overlap):
 
             num_inserts_right_of_read = 0
@@ -682,30 +684,6 @@ def parallel_run_one_window(
             )
             _write_to_file([line], f"coverage_{idx}.txt")
 
-
-
-def update_tiling(tiling, extended_window_mode, max_ins_at_pos):
-    """
-    input tiling:
-
-    return: tiling = [
-            (window_start, original_window_length, control_window_length, counter)
-            for each window
-            ]
-    """
-    update_tiling = []
-
-    for idx, (window_start, window_length) in enumerate(tiling):
-        original_window_length = window_length
-        if extended_window_mode:
-            for pos, val in max_ins_at_pos.items():
-                if window_start <= pos < window_start + original_window_length:
-                    window_length += val
-            update_tiling.append((window_start,original_window_length, window_length))
-        else:
-            update_tiling.append((window_start,original_window_length, window_length))
-
-    return update_tiling
 
 
 def build_windows(alignment_file: str, tiling_strategy: TilingStrategy,
