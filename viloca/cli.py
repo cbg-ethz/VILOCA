7dbb397030a561dc7d9557cc1b0f910676532571
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-11-28 09:20:16 +0100

    [fix[ unexpected keyword argument 'metavar'

--- viloca/cli.py
+++ viloca/cli.py
@@ -201,10 +201,10 @@ def main():
     parser_shotgun.add_argument('--exclude_non_var_pos_threshold', metavar='FLOAT', type=float, dest="exclude_non_var_pos_threshold",
                                 default=-1, help="Runs exclude non-variable positions mode. Set percentage threshold for exclusion.")
 
-    parser_shotgun.add_argument('--reuse_files', metavar='FLOAT', action='store_true', dest="reuse_files",
+    parser_shotgun.add_argument('--reuse_files', action='store_true', dest="reuse_files",
                                 default=-1, help="Enabling this option allows the command line tool to reuse files that were generated in previous runs. When set to true, the tool will check for existing output files and reuse them instead of regenerating the data. This can help improve performance by avoiding redundant file generation processes.")
 
-    parser_shotgun.add_argument('--record_history', metavar='FLOAT', action='store_true', dest="record_history",
+    parser_shotgun.add_argument('--record_history', action='store_true', dest="record_history",
                                 default=-1, help="When enabled, this option saves the history of the parameter values learned during the inference process.")
 
     parser_shotgun.add_argument("--min_windows_coverage", metavar='INT', type=int,
