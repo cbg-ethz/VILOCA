e0d0228f34e7c51125afe4398fe7b19005a65285
LaraFuhrmann <55209716+LaraFuhrmann@users.noreply.github.com>
2024-11-28 11:59:50 +0100

    [e2e tests] run the e2e tests in seperate directories

--- tests/data_1/shotgun_test.sh
+++ tests/data_1/shotgun_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 viloca run -a 0.1 -w 201 --mode shorah -x 100000 -p 0.9 -c 0 \
--r HXB2:2469-3713 -R 42 -f test_ref.fasta -b test_aln.cram --out_format csv "$@"
+-r HXB2:2469-3713 -R 42 -f ../test_ref.fasta -b ../test_aln.cram --out_format csv "$@"
